1d1288abae61123b0046fb7441eb3124d1f54c1f
snappycreeper <anhochanadelflip@gmail.com>
2022-03-31 11:13:16 -0500

    Update COlorS

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2852,9 +2852,9 @@ var templateObject_1$K;
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#aa8929",
-    primaryBright: "#aa8929",
-    primaryDark: "#aa8929",
+    primary: "#630404",
+    primaryBright: "#630404",
+    primaryDark: "#630404",
     secondary: "#7645D9",
     success: "#31D0AA",
     warning: "#FFB237",
