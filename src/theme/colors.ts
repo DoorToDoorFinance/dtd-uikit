38c42703c4bd4c6d5f69b9a54d810f5951933180
snappycreeper <anhochanadelflip@gmail.com>
2022-03-30 20:37:24 -0500

    Update Colors

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -25,7 +25,7 @@ export const lightColors: Colors = {
   tertiary: "#EFF4F5",
   text: "#742fb5",
   textDisabled: "#BDC2C4",
-  textSubtle: "#8f80ba",
+  textSubtle: "#ad1d1d",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
